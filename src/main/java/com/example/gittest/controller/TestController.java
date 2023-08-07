3c96685a2e79efd5cac9625fe13e5f4f3a05b2e0
un <66024439+mapzine123@users.noreply.github.com>
2023-08-08 01:40:49 +0900

    dfdf
    dfdf

--- src/main/java/com/example/gittest/controller/TestController.java
+++ src/main/java/com/example/gittest/controller/TestController.java
@@ -1,4 +1,17 @@
 package com.example.gittest.controller;
 
 public class TestController {
+    private String test;
+
+    public TestController(String test) {
+        this.test = test;
+    }
+
+    public void objectTest() {
+        System.out.println("object Test!");
+    }
+
+    public void printTest() {
+        System.out.println(test);
+    }
 }
